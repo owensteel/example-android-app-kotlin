ced721a8c5f55852309c6ea5ab8f7eef1a10fb17
Owen A Steel <woofdog04@gmail.com>
2025-07-23 22:37:58 +0100

    Removes redundant dependencies

--- app/src/main/java/com/owensteel/starlingroundup/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/owensteel/starlingroundup/viewmodel/MainViewModel.kt
@@ -1,7 +1,5 @@
 package com.owensteel.starlingroundup.viewmodel
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.MutableStateFlow
