6fe258e2dc5bccba2521e353222748bfbbfdff0f
Owen A Steel <woofdog04@gmail.com>
2025-07-23 14:08:34 +0100

    Creates basic Activity screen for our function

--- app/src/main/java/com/owensteel/starlingroundup/MainActivity.kt
+++ app/src/main/java/com/owensteel/starlingroundup/MainActivity.kt
@@ -4,44 +4,28 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.activity.viewModels
+import com.owensteel.starlingroundup.ui.MainScreen
 import com.owensteel.starlingroundup.ui.theme.StarlingRoundupTheme
+import com.owensteel.starlingroundup.viewmodel.MainViewModel
 
+/*
+
+    The entry point for the application
+    Since in this project we only want to fulfil one function for
+    the user, this directly presents our single UI feature
+
+ */
 class MainActivity : ComponentActivity() {
+    private val viewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             StarlingRoundupTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                MainScreen(viewModel)
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    StarlingRoundupTheme {
-        Greeting("Android")
     }
 }
