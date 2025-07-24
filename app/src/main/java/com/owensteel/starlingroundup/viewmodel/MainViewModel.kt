c679b22eddcc336352b23001ea7cd23eb01549e2
Owen A Steel <woofdog04@gmail.com>
2025-07-24 15:24:38 +0100

    Implements API calls in view model, to get account details and weekly transactions feed

--- app/src/main/java/com/owensteel/starlingroundup/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/owensteel/starlingroundup/viewmodel/MainViewModel.kt
@@ -1,11 +1,16 @@
 package com.owensteel.starlingroundup.viewmodel
 
+import android.content.Context
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.owensteel.starlingroundup.model.AccountResponse
+import com.owensteel.starlingroundup.model.TransactionFeedResponse
+import com.owensteel.starlingroundup.network.StarlingService
+import com.owensteel.starlingroundup.token.TokenManager
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
+import retrofit2.Response
 
 /*
 
@@ -13,32 +18,68 @@ import kotlinx.coroutines.launch
 
  */
 
-class MainViewModel : ViewModel() {
-
-    // TODO: Possibly make default value a reference?
+class MainViewModel(
+    private val context: Context,
+    private val tokenManager: TokenManager
+) : ViewModel() {
 
     // Use StateFlow to maintain an observable mutable state for our
     // value that may change between appearances
-    private val _roundUpAmount = MutableStateFlow("£0.00")
-    val roundUpAmount: StateFlow<String> = _roundUpAmount.asStateFlow()
+    private val _roundUpAmountUiState = MutableStateFlow<String>("£0.00")
+    val roundUpAmountUiState: StateFlow<String> = _roundUpAmountUiState
 
-    // Make sure the result is always ready to be seen by the user, so
-    // run it when initialised
-    init {
-        loadRoundUpAmount()
-    }
+    private val _feedState = MutableStateFlow<TransactionFeedResponse?>(null)
+    val feedState: StateFlow<TransactionFeedResponse?> = _feedState
 
-    private fun loadRoundUpAmount() {
+    // 0. Fetch account details
+    private var accountUid: String? = null
+    private var categoryUid: String? = null
+
+    private fun initialiseAccountDetails() {
         viewModelScope.launch {
-            // TODO: Call API, calc round up
-            _roundUpAmount.value = "£0.00"
+            val accountResponse: Response<AccountResponse> = StarlingService.getAccountDetails(
+                context, tokenManager
+            )
+            if (accountResponse.isSuccessful) {
+                accountResponse.body()?.accounts?.firstOrNull()?.let { account ->
+                    accountUid = account.accountUid
+                    categoryUid = account.defaultCategory
+                }
+            }
         }
     }
 
+    // 1. Fetch this week's transactions
+    private fun fetchWeeklyTransactions() {
+        if (accountUid == null || categoryUid == null) return
+
+        viewModelScope.launch {
+            val transactionFeedResponse: Response<TransactionFeedResponse> =
+                StarlingService.getTransactionsForCurrentWeek(
+                    context,
+                    tokenManager,
+                    accountUid!!,
+                    categoryUid!!
+                )
+            if (transactionFeedResponse.isSuccessful) {
+                _feedState.value = transactionFeedResponse.body()
+            }
+        }
+    }
+
+    // 2. Calculate the round-up
+
+    // 3. Perform the transfer
     fun performTransfer() {
         viewModelScope.launch {
             // TODO: Trigger savings goal transfer
         }
+    }
+
+    // Run as soon as initialised
+    init {
+        initialiseAccountDetails()
+        fetchWeeklyTransactions()
     }
 
 }
