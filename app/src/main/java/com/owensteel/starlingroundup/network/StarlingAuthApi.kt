838968211044ba0cceffe0c31fddef0227f07ad0
Owen A Steel <woofdog04@gmail.com>
2025-07-24 13:29:45 +0100

    Adds auth endpoints and models

---
+++ app/src/main/java/com/owensteel/starlingroundup/network/StarlingAuthApi.kt
@@ -0,0 +1,20 @@
+package com.owensteel.starlingroundup.network
+
+import com.owensteel.starlingroundup.model.TokenResponse
+import retrofit2.Response
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.POST
+
+interface StarlingAuthApi {
+
+    @FormUrlEncoded
+    @POST("oauth/token")
+    suspend fun refreshAccessToken(
+        @Field("grant_type") grantType: String = "refresh_token",
+        @Field("refresh_token") refreshToken: String,
+        @Field("client_id") clientId: String,
+        @Field("client_secret") clientSecret: String
+    ): Response<TokenResponse>
+
+}
