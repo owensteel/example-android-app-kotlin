196b0ca081f593ea9b7fe6daa5ec57753fc20332
Owen A Steel <woofdog04@gmail.com>
2025-07-25 00:06:13 +0100

    Adds roundUp method to Money data class

--- app/src/main/java/com/owensteel/starlingroundup/model/Money.kt
+++ app/src/main/java/com/owensteel/starlingroundup/model/Money.kt
@@ -15,10 +15,12 @@ data class Money(
     val currency: String,
     val minorUnits: Long
 ) {
+    // Formatting
     override fun toString(): String {
         val unitSymbol = try {
-            Currency.getInstance(currency).getSymbol(Locale.UK) // locale just determines how symbols are rendered
-        } catch(e:Exception){
+            Currency.getInstance(currency)
+                .getSymbol(Locale.UK) // locale just determines how symbols are rendered
+        } catch (e: Exception) {
             currency
         }
 
@@ -26,5 +28,14 @@ data class Money(
         val pence = minorUnits % 100
 
         return String.format(Locale.UK, "%s%d.%02d", unitSymbol, pounds, pence)
+    }
+
+    // Round up to next pound (in minor units)
+    fun roundUp(): Long {
+        // Is the transaction amount a full pound?
+        val remainder = minorUnits % 100
+        // If not a full pound, return the difference
+        // between it and a full pound
+        return if (remainder == 0L) 0L else (100 - remainder)
     }
 }
