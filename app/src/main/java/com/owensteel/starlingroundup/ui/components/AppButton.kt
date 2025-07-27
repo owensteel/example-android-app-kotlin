cf5834efd4eea4574bbfb3a1deb810906f631c1d
Owen A Steel <woofdog04@gmail.com>
2025-07-27 14:31:33 +0100

    Adds secondary button

--- app/src/main/java/com/owensteel/starlingroundup/ui/components/AppButton.kt
+++ app/src/main/java/com/owensteel/starlingroundup/ui/components/AppButton.kt
@@ -6,6 +6,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 
 @Composable
 fun AppButton(
@@ -19,6 +20,24 @@ fun AppButton(
         colors = ButtonDefaults.buttonColors(
             containerColor = MaterialTheme.colorScheme.secondary,
             contentColor = MaterialTheme.colorScheme.onSecondary
+        )
+    ) {
+        Text(text)
+    }
+}
+
+@Composable
+fun AppSecondaryButton(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    text: String
+) {
+    Button(
+        onClick = onClick,
+        modifier = modifier,
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Color.Transparent,
+            contentColor = MaterialTheme.colorScheme.onSurface
         )
     ) {
         Text(text)
