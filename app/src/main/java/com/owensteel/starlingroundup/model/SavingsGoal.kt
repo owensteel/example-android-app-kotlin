f83975c302bfe7431bbe56cd0ea744d20e11ebdf
Owen A Steel <woofdog04@gmail.com>
2025-07-27 15:56:12 +0100

    Savings Goal target can sometimes be null

--- app/src/main/java/com/owensteel/starlingroundup/model/SavingsGoal.kt
+++ app/src/main/java/com/owensteel/starlingroundup/model/SavingsGoal.kt
@@ -3,7 +3,7 @@ package com.owensteel.starlingroundup.model
 data class SavingsGoal(
     val savingsGoalUid: String,
     val name: String,
-    val target: Money,
+    val target: Money?,
     val totalSaved: Money,
     val savedPercentage: Int,
     val state: String
