f1c44f85b9ac44b47665bf98ada6a475f18ec407
Owen A Steel <woofdog04@gmail.com>
2025-07-29 14:40:30 +0100

    Changes round-up completed icon from 'Check' to identical but more appropriate 'Done'

--- app/src/main/java/com/owensteel/starlingroundup/ui/roundup/TransactionsFeedRow.kt
+++ app/src/main/java/com/owensteel/starlingroundup/ui/roundup/TransactionsFeedRow.kt
@@ -9,7 +9,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Done
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -134,8 +134,8 @@ fun TransactionFeedRow(
                     // Tick icon to show user this round-up has
                     // been transferred
                     Icon(
-                        imageVector = Icons.Default.Check,
-                        contentDescription = Icons.Default.Check.name,
+                        imageVector = Icons.Default.Done,
+                        contentDescription = Icons.Default.Done.name,
                         modifier = Modifier.size(20.dp)
                     )
                 }
