fb141d4d781133e0fbeb3d9e7ca6ee8572959903
Owen A Steel <woofdog04@gmail.com>
2025-07-30 21:43:11 +0100

    Tests RoundUpCutoffTimestampStore

---
+++ app/src/test/java/com/owensteel/starlingroundup/data/local/RoundUpCutoffTimestampStore.kt
@@ -0,0 +1,30 @@
+package com.owensteel.starlingroundup.data.local
+
+import androidx.datastore.preferences.core.stringPreferencesKey
+import com.owensteel.starlingroundup.util.SharedConstants.PreferenceKeys.LATEST_ROUNDUP_CUTOFF_TIMESTAMP
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.verify
+
+class RoundUpCutoffTimestampStoreTest {
+
+    private val dataStoreManager: DataStoreManager = mock()
+    private lateinit var store: RoundUpCutoffTimestampStore
+
+    @Before
+    fun setup() {
+        store = RoundUpCutoffTimestampStore(mock(), dataStoreManager)
+    }
+
+    @Test
+    fun `saveLatestRoundUpCutoffTimestamp saves correctly`() = runTest {
+        store.saveLatestRoundUpCutoffTimestamp("2025-07-30T10:00:00Z")
+
+        verify(dataStoreManager).write(
+            stringPreferencesKey(LATEST_ROUNDUP_CUTOFF_TIMESTAMP),
+            "2025-07-30T10:00:00Z"
+        )
+    }
+}
